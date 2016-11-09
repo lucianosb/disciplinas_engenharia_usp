7ca60d13d5807b98aa6beeea5423efe7a9694ec3
Luciano Santa Brígida <lucianosb@users.noreply.github.com>
2016-11-09 13:18:05 -0200

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -2,16 +2,16 @@
 // This is a template for a PHP scraper on morph.io (https://morph.io)
 // including some code snippets below that you should find helpful
 
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
+ require 'scraperwiki.php';
+ require 'scraperwiki/simple_html_dom.php';
 //
 // // Read in a page
 $html = scraperwiki::scrape("https://uspdigital.usp.br/jupiterweb/listarGradeCurricular?codcg=3&codcur=3021&codhab=100&tipo=N");
 //
 // // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
+ $dom = new simple_html_dom();
+ $dom->load($html);
+ print_r($dom->find("a.link_grey"));
 //
 // // Write out to the sqlite database using scraperwiki library
 // scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
